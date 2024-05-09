8180a04235ece3b9437edc7d2eb5a5db771ee7b5
Gayathri Girish Nair <ggnair2000@gmail.com>
2024-05-09 15:49:40 +0100

    added main.R file

--- Code/model_selection.R
+++ Code/model_selection.R
@@ -1,13 +1,9 @@
-# Import libraries.
-library(rpart)
-library(dplyr)
-library(ggplot2) # CART
-library(partykit) # CART
-library(pscl) # Zero inflated poisson model.
-library(MASS) # Poisson and negative binomial models.
-library(caret) # For cross validation.
+# LOAD PACKAGES
+require(pscl) # Zero inflated poisson model.
+require(MASS) # Poisson and negative binomial models.
+require(caret) # For cross validation.
 
-### Utility Functions
+# HELPER FUNCTIONS
 mse <- function(y, y_pred) {
   # Computes mean squared error.
   # @param y: Truth.
@@ -20,76 +16,134 @@ mse <- function(y, y_pred) {
 }
 
 cv10fold <- function(model_type, data) {
-  # Returns average MSE after 10 fold cross validation.
-  # @param model_type: Name of the type of model to use.
-  # @param data: Data to perform 10 fold cv using.
-  # @return: Average of MSE values across 10 folds.
-  res_cv = mean(as.numeric(lapply(folds, function(index_validate) {
-    fold_train <- data_train[-index_validate, ]
-    fold_validate <- data_train[index_validate, ]
-    if (model_type == 'poisson') {
-      model <- glm(Total.Uninjured ~ ., data=fold_train, family="poisson")
-    } else if (model_type == 'negbin') {
-      model <- glm.nb(Total.Uninjured ~ ., data=fold_train)
-    } else if (model_type == 'zip') {
-      model <- zeroinfl(Total.Uninjured ~ ., data=fold_train, dist="poisson")
-    } else if (model_type == 'znib') {
-      model <- zeroinfl(Total.Uninjured ~ ., data=fold_train, dist="negbin")
-    } else if (model_type == 'hurdle_p') {
-      model <- zeroinfl(Total.Uninjured ~ ., data=fold_train, dist="negbin")
-    } else if (model_type == 'hurdle_negbin') {
-      model <- hurdle(Total.Uninjured ~ ., data = data, dist = "negbin")
-    } else { # Invalid model.
-      return (-1)
+  ### Returns average MSE after 10 fold cross validation.
+  ### @param model_type: Name of the type of model to use.
+  ### @param data: Data to perform 10 fold CV using.
+  ### @return: Average of MSE values across 10 folds.
+  res_cv = mean(as.numeric(lapply(
+    folds, function(index_validate) {
+      fold_train <- data_train[-index_validate, ]
+      fold_validate <- data_train[index_validate, ]
+      if (model_type == 'p') {
+        model <- glm(
+          Total.Uninjured ~ ., 
+          data=fold_train, 
+          family="poisson"
+        )
+      } else if (model_type == 'nb') {
+        model <- glm.nb(
+          Total.Uninjured ~ ., 
+          data=fold_train
+        )
+      } else if (model_type == 'zip') {
+        model <- zeroinfl(
+          Total.Uninjured ~ ., 
+          data=fold_train, 
+          dist="poisson"
+        )
+      } else if (model_type == 'znib') {
+        model <- zeroinfl(
+          Total.Uninjured ~ ., 
+          data=fold_train, 
+          dist="negbin"
+        )
+      } else if (model_type == 'hp') {
+        model <- zeroinfl(
+          Total.Uninjured ~ ., 
+          data=fold_train, 
+          dist="negbin"
+        )
+      } else if (model_type == 'hnb') {
+        model <- hurdle(
+          Total.Uninjured ~ ., 
+          data = data, 
+          dist = "negbin"
+        )
+      } else { # Invalid model.
+        return (-1)
+      }
+      y <- fold_train$Total.Uninjured
+      y_pred <- predict(
+        model, 
+        newdata=fold_train, 
+        type="response"
+      )
+      error <- mse(y, y_pred)
+      return(error)
     }
-    y <- fold_train$Total.Uninjured
-    y_pred <- predict(model, newdata=fold_train, type="response")
-    error <- mse(y, y_pred)
-    return(error)
-  }))) # Cross validation result.
+  ))) # Cross validation result.
   return(res_cv)
 }
 
-### Main
+# MAIN
 
 # Set working directory to this one.
 setwd("C:/Users/g_gna/Documents/TCD/Modules/CS7DS1_DataAnalytics/Project/Code")
 
 # Load data.
-data <- read.csv("../Data/data_clean.csv", header=TRUE, sep=",")
+data <- read.csv(
+  "../Data/data_clean.csv", 
+  header=TRUE, sep=","
+)
 
 # 1. Poisson Model
-model_poisson <- glm(Total.Uninjured ~ ., data=data, family="poisson")
+model_poisson <- glm(
+  Total.Uninjured ~ ., 
+  data=data, family="poisson"
+)
 
 # 2. Negative Binomial Model
-model_negbin <- glm.nb(Total.Uninjured ~ ., data=data)
+model_negbin <- glm.nb(
+  Total.Uninjured ~ ., 
+  data=data
+)
 
 # 3. Zero-Inflated Models
-model_zip <- zeroinfl(Total.Uninjured ~ ., data=data, dist="poisson")
-model_zinb <- zeroinfl(Total.Uninjured ~ ., data=data, dist="negbin")
+model_zip <- zeroinfl(
+  Total.Uninjured ~ ., 
+  data=data, dist="poisson"
+)
+model_zinb <- zeroinfl(
+  Total.Uninjured ~ ., 
+  data=data, dist="negbin"
+)
 
 # 4. Hurdle Model
-model_hurdle_p <- hurdle(Total.Uninjured ~ ., data=data, dist="poisson")
-model_hurdle_nb <- hurdle(Total.Uninjured ~ ., data=data, dist="negbin")
+model_hurdle_p <- hurdle(
+  Total.Uninjured ~ ., 
+  data=data, dist="poisson"
+)
+model_hurdle_nb <- hurdle(
+  Total.Uninjured ~ ., 
+  data=data, dist="negbin"
+)
 
 ### 10 Fold Cross validation.
 
 # 1. Train - test split.
-index_train <- createDataPartition(data$Total.Uninjured, p = 0.8, list = FALSE)
+index_train <- createDataPartition(
+  data$Total.Uninjured, 
+  p = 0.8, list = FALSE
+)
 data_train <- data[index_train, ]
 data_test <- data[-index_train, ]
 
 # 2. Create 10 folds of the train data set.
 get_best_model <- function(data) {
   print('Determining best model through 10 Fold CV.')
-  model_types <- list('poisson', 'negbin', 'zip', 'zinb', 'hurdle_p', 'hurdle_negbin')
+  model_types <- list(
+    'p', 'nb', 'zip', 'zinb', 'hp', 'hnb'
+  )
   res_cv <- list()
   for (model_type in model_types) {
     res_cv <- append(res_cv, cv10fold(model_type, data))
     print(paste('Evaluated model:', model_type))
   }
   model_best <- model_types[which.min(res_cv)]
-  print(paste('Model resulting in lowest avg. MSE across 10 folds =', model_best))
+  print(paste(
+    'Model resulting in lowest avg.',
+    'MSE across 10 folds =', model_best
+  ))
   return(model_best)
 }
 best_model <- get_best_model()
